0d9aa38db3f5072ccea5fed32545ff851f97cbe0
Eelco Koster <eelco.koster@gmail.com>
2023-07-21 19:14:25 +0200

    Add SonarCloudConsole

---
+++ src/SonarCloudConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace SonarCloudConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string componentName = "vitas-development-webproject";
+
+            var measurements = SonarCloud.GetMeasurements(componentName);
+            var projectMeasurements = new ProjectMeasurements(measurements);
+            Console.WriteLine($"Project summary for {componentName}:");
+            Console.WriteLine($"The project consists of {projectMeasurements.LinesOfCode} lines of code.");
+            Console.WriteLine($"The unittest of this project covers {projectMeasurements.Coverage}% of the code.");
+            Console.WriteLine($"The project has {projectMeasurements.DuplicatedLinesDensity} duplicated lines of density.");
+            Console.WriteLine($"SonarCloud found {projectMeasurements.SecurityHotspots} security hotspots, {projectMeasurements.Bugs} bugs and {projectMeasurements.CodeSmells} code smells which need to be addressed.");
+
+            Console.WriteLine("---------------");
+
+            var issues = SonarCloud.GetIssues(componentName);
+            foreach (var issue in issues)
+            {
+                Console.WriteLine($"{issue.severity}: {issue.message}");
+                string file = issue.component.Substring(issue.component.LastIndexOf(':') + 1);
+                Console.WriteLine($" File: {file} in line {issue.line}");
+            }
+        }
+    }
+
+    public class ProjectMeasurements
+    {
+        List<Measure> _measturements;
+        public ProjectMeasurements(List<Measure> measturements)
+        {
+            _measturements = measturements;
+        }
+
+        public int? Bugs { get { return Convert.ToInt32(_measturements.FirstOrDefault(m => m.metric == "bugs")?.value); } }
+        public string Coverage { get { return _measturements.FirstOrDefault(m => m.metric == "coverage")?.value; } }
+        public int? CodeSmells { get { return Convert.ToInt32(_measturements.FirstOrDefault(m => m.metric == "code_smells")?.value); } }
+        public string DuplicatedLinesDensity { get { return _measturements.FirstOrDefault(m => m.metric == "duplicated_lines_density")?.value; } }
+        public int? LinesOfCode { get { return Convert.ToInt32(_measturements.FirstOrDefault(m => m.metric == "ncloc")?.value); } }
+        public int? SecurityHotspots { get { return Convert.ToInt32(_measturements.FirstOrDefault(m => m.metric == "security_hotspots")?.value); } }
+    }
+}
---
+++ src/SonarCloudConsole/SonarCloud.cs
@@ -0,0 +1,118 @@
+﻿using Newtonsoft.Json;
+
+namespace SonarCloudConsole
+{
+    public class SonarCloud
+    {
+        public static List<Issue> GetIssues(string component)
+        {
+            var result = new List<Issue>();
+            var issues = GetIssues(component, 1);
+            result.AddRange(issues.issues);
+            var total = issues.ps * issues.p;
+            while (issues != null && issues.paging.total > total)
+            {
+                issues = GetIssues(component, issues.paging.pageIndex + 1);
+                result.AddRange(issues.issues);
+                total = issues.ps * issues.p;
+            }
+            return result;
+        }
+
+        public static List<Measure> GetMeasurements(string component)
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync($"https://sonarcloud.io/api/measures/component?component={component}&metricKeys=bugs,code_smells,coverage,duplicated_lines_density,ncloc,security_hotspots,sqale_index,vulnerabilities").Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+            var measurements = JsonConvert.DeserializeObject<Measurements>(content);
+            return measurements?.component.measures;
+        }
+
+        private static Issues GetIssues(string component, int page)
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync($"https://sonarcloud.io/api/issues/search?componentKeys={component}&ps=100&p={page}").Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+            var issues = JsonConvert.DeserializeObject<Issues>(content);
+            return issues;
+        }
+    }
+
+
+    public class Issue
+    {
+        public string key { get; set; }
+        public string rule { get; set; }
+        public string severity { get; set; }
+        public string component { get; set; }
+        public string project { get; set; }
+        public int line { get; set; }
+        public string hash { get; set; }
+        public TextRange textRange { get; set; }
+        public List<object> flows { get; set; }
+        public string status { get; set; }
+        public string message { get; set; }
+        public string effort { get; set; }
+        public string debt { get; set; }
+        public string assignee { get; set; }
+        public List<string> tags { get; set; }
+        public DateTime creationDate { get; set; }
+        public DateTime updateDate { get; set; }
+        public string type { get; set; }
+        public string organization { get; set; }
+        public string externalRuleEngine { get; set; }
+    }
+
+    public class Organization
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Paging
+    {
+        public int pageIndex { get; set; }
+        public int pageSize { get; set; }
+        public int total { get; set; }
+    }
+
+    public class Issues
+    {
+        public int total { get; set; }
+        public int p { get; set; }
+        public int ps { get; set; }
+        public Paging paging { get; set; }
+        public int effortTotal { get; set; }
+        public int debtTotal { get; set; }
+        public List<Issue> issues { get; set; }
+    }
+
+    public class TextRange
+    {
+        public int startLine { get; set; }
+        public int endLine { get; set; }
+        public int startOffset { get; set; }
+        public int endOffset { get; set; }
+    }
+
+    public class Component
+    {
+        public string id { get; set; }
+        public string key { get; set; }
+        public string name { get; set; }
+        public string qualifier { get; set; }
+        public List<Measure> measures { get; set; }
+    }
+
+    public class Measure
+    {
+        public string metric { get; set; }
+        public string value { get; set; }
+        public bool bestValue { get; set; }
+    }
+
+    public class Measurements
+    {
+        public Component component { get; set; }
+    }
+}
